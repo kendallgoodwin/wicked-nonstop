654af53f958a78244efaa9b1b775989a8d9b36d5
kendallgoodwin <kendall@kendallgoodwin.com>
2016-09-15 11:55:05 -0700

    media queries

---
+++ script.js
@@ -0,0 +1,7 @@
+// window.onload = choosePic;
+
+// var images = new Array("images/grendall.jpg","images/grendall2.jpg","images/grendall3.jpg", "grendall4.jpg");
+
+// function choosePic() {
+//      var randomNum = Math.floor(Math.random() * images.length);
+//      document.getElementById("myPicture").src = images[randomNum];
